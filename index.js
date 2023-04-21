5252cd4b7feea160d8939f806258bd9496699e72
Babul Akter <babulakterfsd@gmail.com>
2023-04-22 00:42:39 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  if(req.url === '/json') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify({id: 1, name: 'John Doe', salary: 3000}));
+    res.end();
+  } else if(req.url === '/html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<p>Home Page</p>');
+    res.end();
+  }
+});
+
+// Listen on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
